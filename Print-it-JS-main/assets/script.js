c5da13e41b8098ec5eeeb6228cbecdea3f3a1afe
Blerina79 <graines.de.tomates@gmail.com>
2024-02-13 10:29:10 +0100

    carousel

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -17,7 +17,39 @@ const slides = [
 	}
 ]
 
- 
+// Initialisation de la position actuelle du carrousel//
+let position = 0;
+const numberOfSlides = slides.length;
+// Fonction pour mettre à jour le carrousel //
+const updateCarousel = () => {
+    const element = slides[position];
+    document.querySelector(".banner-img").setAttribute("src", "./assets/images/slideshow/" + element.image);
+    document.querySelector(".banner-txt").innerHTML = element.tagLine;
+    document.querySelectorAll(".dot").forEach((dot, index) => {
+        dot.classList.toggle('dot_selected', index === position);
+    });
+};
+  // Calcul de la nouvelle position. L'opérateur modulo garantit que la position reste dans les limites valides //
+const changePosition = (step) => {
+    position = (position + step + numberOfSlides) % numberOfSlides;
+    updateCarousel();
+};
+ // Ajout de gestionnaires d'événements pour les flèches de navigation //
+document.querySelector('.arrow_left').addEventListener("click", () => changePosition(-1));
+document.querySelector('.arrow_right').addEventListener("click", () => changePosition(1));
+// Fonction pour créer les points de navigation (dots) du carrousel //
+const createDots = () => {
+    const dotsContainer = document.querySelector(".dots");
+   // Création d'un point pour chaque slide //
+    slides.forEach(() => {
+        const dot = document.createElement("div");
+        dot.className = "dot";
+        dotsContainer.appendChild(dot);
+    });
+};
+ // Initialisation des points de navigation et mise à jour initiale du carrousel //
+createDots();
+updateCarousel();
 
 
 
